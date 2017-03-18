9ce7477769466a2f218c1b77253a0f82828f82bb
Brian Bensky <brianbensky@gmail.com>
2017-03-18 16:46:15 -0600

    medium 2 testing

--- launch_school_lessons/130/130_small_problems/medium_2_testing/transaction.rb
+++ launch_school_lessons/130/130_small_problems/medium_2_testing/transaction.rb
@@ -7,15 +7,27 @@ class Transaction
     @amount_paid = 0
   end
 
-  def prompt_for_payment(input: $stdin, output: $stdout)
-    loop do
-      output.puts "You owe $#{item_cost}.\nHow much are you paying?" 
-      @amount_paid = input.gets.chomp.to_f 
-      break if valid_payment? && sufficient_payment?
-      puts 'That is not the correct amount. ' \
-           'Please make sure to pay the full cost.'
+  # def prompt_for_payment(input: $stdin, output: $stdout)
+    # loop do
+    #   output.puts "You owe $#{item_cost}.\nHow much are you paying?"
+    #   @amount_paid = input.gets.chomp.to_f
+    #   break if valid_payment? && sufficient_payment?
+    #   puts 'That is not the correct amount. ' \
+    #        'Please make sure to pay the full cost.'
+    # end
+  # end
+
+  # alt w/o output
+
+    def prompt_for_payment(input: $stdin)
+      loop do
+        puts "You owe $#{item_cost}.\nHow much are you paying?"
+        @amount_paid = input.gets.chomp.to_f
+        break if valid_payment? && sufficient_payment?
+        puts 'That is not the correct amount. ' \
+             'Please make sure to pay the full cost.'
     end
-  end
+      end
 
   private
 
--- launch_school_lessons/130/130_small_problems/medium_2_testing/transaction_test.rb
+++ launch_school_lessons/130/130_small_problems/medium_2_testing/transaction_test.rb
@@ -2,11 +2,22 @@ require 'minitest/autorun'
 require_relative 'transaction'
 
 class TransactionTest < Minitest::Test
+  # def test_prompt_for_payment
+  #   input = StringIO.new('600')
+  #   output = StringIO.new
+  #   transaction = Transaction.new(500)
+  #   transaction.prompt_for_payment(input: input, output: output)
+
+  #   assert_equal 600, transaction.amount_paid
+  # end
+
+  # alt using capture_io
+
   def test_prompt_for_payment
-    input = StringIO.new("600")
-    output = StringIO.new
+    input = StringIO.new('600')
     transaction = Transaction.new(500)
-    transaction.prompt_for_payment(input: input, output: output)
+
+    capture_io { transaction.prompt_for_payment(input: input) }
 
     assert_equal 600, transaction.amount_paid
   end
