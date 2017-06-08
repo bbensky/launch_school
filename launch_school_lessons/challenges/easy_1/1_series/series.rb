ff1f6f6f207d41616918ed6b32c7f944debd7956
Brian Bensky <brianbensky@gmail.com>
2017-06-07 21:44:52 -0600

    clean series

--- launch_school_lessons/challenges/easy_1/1_series/series.rb
+++ launch_school_lessons/challenges/easy_1/1_series/series.rb
@@ -1,16 +1,3 @@
-# class Series
-#   def initialize(numbers)
-#     @digits = numbers.split('').map(&:to_i)
-#   end
-
-#   def slices(cons)
-#     raise ArgumentError, 'number larger than string size' if cons > @digits.size
-#     @digits.each_cons(cons).with_object([]) { |group, result| result << group}
-#   end
-# end
-
-# alt shorter each_cons line
-
 class Series
   def initialize(numbers)
     @digits = numbers.split('').map(&:to_i)
